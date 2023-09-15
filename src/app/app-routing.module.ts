41b6e8480c73cc6e0c71c76c95b5326d3622738e
Franco Lippi <soyflippi@gmail.com>
2023-09-14 21:55:21 -0300

    2023 - Update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
 import { HomeComponent } from './modules/general/home/home.component';
 import { NotFoundComponent } from './modules/general/not-found/not-found.component';
+
 
 const routes: Routes = [
   { path: '', component: HomeComponent, },
